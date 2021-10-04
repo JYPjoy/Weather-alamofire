e314be7d701026432b6e9b5a88fa4ba3b0a6e0a7
JYP <jypjoy@gmail.com>
2021-10-04 14:56:31 +0900

    Initialize project

---
+++ Weather/Weather/WeatherViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Jiyoung Park on 2021/10/04.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addLocationButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func locaionButtonTapped(_ sender: Any) {
+    }
+}
+
