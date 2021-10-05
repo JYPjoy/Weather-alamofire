f954a117b4e85d8922e6215b7d851394261a3258
JYP <jypjoy@gmail.com>
2021-10-05 14:08:18 +0900

    Make conditionI.V changes according to conditionId

---
+++ Weather/Weather/extension/Int+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extensions.swift
+//  Weather
+//
+//  Created by Jiyoung Park on 2021/10/05.
+//
+
+import Foundation
+
+extension Int {
+    func toString() -> String {
+        return "\(self)"
+    }
+}
--- Weather/Weather/manager/WeatherManager.swift
+++ Weather/Weather/manager/WeatherManager.swift
@@ -12,7 +12,7 @@ struct WeatherManager {
     
     private let API_KEY = "e7379996276b09c2b07ceb4e5cd01e57"
     
-    func fetchWeather(byCity city: String, completion: @escaping(Result<WeatherData, Error>) -> Void) {
+    func fetchWeather(byCity city: String, completion: @escaping(Result<WeatherModel, Error>) -> Void) {
         
         let query = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? city
         let path = "https://api.openweathermap.org/data/2.5/weather?q=%@&appid=%@&units=metric"
@@ -21,7 +21,8 @@ struct WeatherManager {
         AF.request(urlString).responseDecodable(of: WeatherData.self, queue: .main, decoder: JSONDecoder()) {(response) in
             switch response.result {
             case .success(let weatherData):
-                completion(.success(weatherData))
+                let model = weatherData.model
+                completion(.success(model))
             case .failure(let error):
                 completion(.failure(error))
             }
